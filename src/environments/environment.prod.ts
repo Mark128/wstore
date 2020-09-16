4ede312849aaaa16575a2f98275b2281e94728c0
Iteis128 <m_antonaidis@live.com>
2020-09-15 20:53:14 -0400

    set up firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAVSzDK8DHtWbfFAkY8sgF446bQ0yGJQy4",
+    authDomain: "weedstore-ffd3a.firebaseapp.com",
+    databaseURL: "https://weedstore-ffd3a.firebaseio.com",
+    projectId: "weedstore-ffd3a",
+    storageBucket: "weedstore-ffd3a.appspot.com",
+    messagingSenderId: "101948445455",
+    appId: "1:101948445455:web:5b95f21a36982ace6f647a",
+    measurementId: "G-S29FZD8GR0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAVSzDK8DHtWbfFAkY8sgF446bQ0yGJQy4",
+    authDomain: "weedstore-ffd3a.firebaseapp.com",
+    databaseURL: "https://weedstore-ffd3a.firebaseio.com",
+    projectId: "weedstore-ffd3a",
+    storageBucket: "weedstore-ffd3a.appspot.com",
+    messagingSenderId: "101948445455",
+    appId: "1:101948445455:web:5b95f21a36982ace6f647a",
+    measurementId: "G-S29FZD8GR0"
+  }
 };
 
 /*
